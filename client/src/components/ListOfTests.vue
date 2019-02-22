b1788063da74e0e19df08ac875d44033ed0aa7b4
Jonathan <jtc7537@gmail.com>
2019-02-22 13:56:22 -0500

    tag

--- client/src/components/ListOfTests.vue
+++ client/src/components/ListOfTests.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
-        <div v-for='(test, index) in tests'>
-            <div :key=index>{{test}}</div>
+        <div>
+            <div v-for='(tag, index) in tags' @click='hideClass(tag)' :key='"tag_"+index'>{{tag}}</div>
+        </div>
+        <div v-for='(test, index) in tests' :key="'test_' + index"
+            :class="{ hidden: test.hidden }">
+            <span class='pages-tag tag'>{{test.pages}}</span>
+            <span class='devices-tag tag'>{{test.devices}}</span>
+            <a class='test-link' :href=test.href target='_blank'><div>{{test.name}}</div></a>
         </div>
     </div>
 </template>
@@ -15,16 +21,59 @@ export default {
     props: [],
     data: function() {
         return {
-            tests: ['test 1', 'test 2']
+            tests: [],
+            tags: []
         }
     },
     created: function () {
         axios.get('http://localhost:8081/')
             .then(response => {
-                this.tests = [...this.tests, ...response.data]
+                // this.tests = [...response.data]
+                response.data.forEach(e => {
+                    const d = e.split('-')
+                    const test = {
+                        href: `http://localhost:8081/api/pdf/${e.replace(' ','_')}`,
+                        pages: d[0],
+                        name: d[1],
+                        devices: d[2].replace('.pdf',''),
+                        hidden: false
+                    }
+                    this.tests.push(test)
+                    !this.tags.includes(test.pages) && this.tags.push(test.pages)
+                    !this.tags.includes(test.devices) && this.tags.push(test.devices)
+                })
                 console.log(response)
             })
 
+    },
+    methods: {
+        hideClass: function (t) {
+            console.log(t)
+            this.tests.forEach((e, i) => {
+                if(e.devices == t) this.tests[i].hidden = !this.tests[i].hidden
+                if(e.pages == t) this.tests[i].hidden = !this.tests[i].hidden
+            })
+        }
     }
 }
 </script>
+
+<style>
+    .test-link {
+        display: inline-block;
+        margin: 8px
+    }
+
+    .tag {
+        font-size: .5rem;
+        font-weight: bold;
+        background-color: black;
+        color: white;
+        margin: 5px;
+        padding: 2px 8px
+    }
+
+    .hidden {
+        display: none
+    }
+</style>
--- controllers/api.js
+++ controllers/api.js
@@ -1,8 +1,14 @@
 const readFiles = require('./readFiles')
+const path = require('path')
 
 module.exports = function(app){
     app.get('/', (req, res) => {
         const files = readFiles()
         res.send(files)
+    })
+    app.get('/api/pdf/:file', (req,res) => {
+        const p = req.params.file.replace('_',' ').replace('%20','_')
+        console.log(p)
+        res.sendFile(path.join(__dirname,`./../tests/${p}`))
     })
 }
